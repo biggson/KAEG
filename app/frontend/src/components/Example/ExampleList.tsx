7d0cd8b04d4e349246e61ff2390d7edb2403cb61
biggson <nosazee@yahoo.com>
2024-10-01 16:31:33 +0100

    Initial commit

---
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -0,0 +1,33 @@
+import { Example } from "./Example";
+
+import styles from "./Example.module.css";
+
+export type ExampleModel = {
+    text: string;
+    value: string;
+};
+
+const EXAMPLES: ExampleModel[] = [
+    {
+        text: "What is Professional Skepticism?",
+        value: "What is Professional Skepticism?"
+    },
+    { text: "What is Control Testing?", value: "What is Control Testing?" },
+    { text: "How to perform Control Testing on Automated Control?", value: "How to perform Control Testing on Automated Control?" }
+];
+
+interface Props {
+    onExampleClicked: (value: string) => void;
+}
+
+export const ExampleList = ({ onExampleClicked }: Props) => {
+    return (
+        <ul className={styles.examplesNavList}>
+            {EXAMPLES.map((x, i) => (
+                <li key={i}>
+                    <Example text={x.text} value={x.value} onClick={onExampleClicked} />
+                </li>
+            ))}
+        </ul>
+    );
+};
